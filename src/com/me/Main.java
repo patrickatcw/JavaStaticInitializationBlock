b32d8f281bb26ca66270974c373dd5ab0c81aef2
patrickatcw <pj8392@yahoo.com>
2018-05-03 13:03:35 -0400

    testd/ran, note order of program read, all done

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -4,7 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //step 6 instance
+        System.out.println("Main method called");
+        SIBTest test = new SIBTest();
+        test.someMethod();
+        System.out.println("Owner is " + SIBTest.owner);
+        //then run
+        //results; note order of how everything is called
+        /*
+        Main method called
+        SIBTest static initialization block called
+        2nd initialization block called
+        SIB constructor called
+        someMethod called
+        Owner is Patrick
+         */
     }
-
 
 }
